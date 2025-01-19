926d7c058c0e4f1f9acb6e2b4d948224df7be536
Abhishek Choudhary <pmoabhishek@gmail.com>
2025-01-20 01:25:15 +0530

    feat(middleware, ui): add middleware and create home page

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -65,8 +65,10 @@ function AccountDropdown() {
 
 export function Header() {
   const session = useSession();
+  const isLoggedIn = !!session.data;
+
   return (
-    <header className="bg-gray-100 py-2 dark:bg-gray-900 z-10 relative">
+    <header className="bg-gray-100 py-2 dark:bg-gray-900 mx-auto z-10 relative">
       <div className="container mx-auto flex justify-between items-center">
         <div>
           <Link href="/" className="flex gap-2 items-center hover:underline">
@@ -74,15 +76,22 @@ export function Header() {
           </Link>
         </div>
 
-        <nav>
+        <nav className="flex gap-4">
+          {isLoggedIn && (
+            <>
+          <Link className="hover:underline" href="/browse">
+          Browse
+          </Link>
           <Link className="hover:underline" href="/your-rooms">
             Your Rooms
           </Link>
+          </>
+          )}
         </nav>
 
         <div className="flex items-center space-x-6">
-          {session.data && <AccountDropdown />}
-          {!session.data && (
+          {isLoggedIn && <AccountDropdown />}
+          {!isLoggedIn && (
             <Button onClick={() => signIn()} variant="link">
               <LogInIcon className="mr-2" />
               Sign In
